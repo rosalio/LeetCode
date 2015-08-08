066e5f7769910a70761858f27e3db56da248f7f7
Alex Dai <dai_xiao_chen@hotmail.com>
2015-08-08 01:51:47 -0700

    Initial Commit

---
+++ Algorithms/C++/1_Two_Sum/1_Two_Sum/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  1_Two_Sum
+//
+//  Created by Xiaochen Dai on 8/8/15.
+//  Copyright (c) 2015 Xiaochen Dai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
