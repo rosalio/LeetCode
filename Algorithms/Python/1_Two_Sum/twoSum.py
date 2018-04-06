93d606f96074ddd514e077a631626a2605526cca
Alex Dai <xiaoched@amazon.com>
2018-04-05 23:35:16 -0700

    Clear repository

--- Algorithms/Python/1_Two_Sum/twoSum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    # @return a tuple, (index1, index2)
-    def twoSum(self, num, target):
-        checkTable = {}
-        for i in xrange(0, len(num)):
-            if checkTable.has_key(target - num[i]):
-                return (checkTable.get(target - num[i]) + 1, i + 1)
-            else:
-                checkTable[num[i]] = i
