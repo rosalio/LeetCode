9491c0f2858f92300fd838eff17d4544ef18b0ea
Rosalio <rosalio@users.noreply.github.com>
2019-05-04 23:14:09 -0700

    Update AddTwoNumbers.java

--- Problems/2_Add_Two_Numbers/AddTwoNumbers.java
+++ Problems/2_Add_Two_Numbers/AddTwoNumbers.java
@@ -1,99 +1,61 @@
-/**********************************************************************************************************************
-
-You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
-
-You may assume the two numbers do not contain any leading zero, except the number 0 itself.
-
-Example
-
-Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
-Output: 7 -> 0 -> 8
-Explanation: 342 + 465 = 807.
-
-**********************************************************************************************************************/
-
-class AddTwoNumbers {
-    public static void main(String[] args) {
-        System.out.println("2. Add Two Numbers");
-
-        ListNode a1 = new ListNode(2);
-        ListNode a2 = new ListNode(4);
-        ListNode a3 = new ListNode(3);
-        a1.next = a2;
-        a2.next = a3;
-        a3.next = null;
-
-        ListNode b1 = new ListNode(5);
-        ListNode b2 = new ListNode(6);
-        ListNode b3 = new ListNode(4);
-        b1.next = b2;
-        b2.next = b3;
-        b3.next = null;
-
-        printListNode(a1);
-        printListNode(b1);
-
-        ListNode result = addTwoNumbers(a1, b1);
-        printListNode(result);
-    }
-
-    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode pre = new ListNode(-1);
-        ListNode curr = pre;
-        int carry = 0;
-
-        while (l1 != null && l2 != null) {
-            ListNode newNode = new ListNode(l1.val + l2.val + carry);
-            curr.next = newNode;
-            curr = newNode;
-            if (curr.val >= 10) {
-                curr.val %= 10;
-                carry = 1;
-            } else {
-                carry = 0;
+/*
+ * @lc app=leetcode id=2 lang=java
+ *
+ * [2] Add Two Numbers
+ *
+ * https://leetcode.com/problems/add-two-numbers/description/
+ *
+ * algorithms
+ * Medium (30.81%)
+ * Total Accepted:    812.1K
+ * Total Submissions: 2.6M
+ * Testcase Example:  '[2,4,3]\n[5,6,4]'
+ *
+ * You are given two non-empty linked lists representing two non-negative
+ * integers. The digits are stored in reverse order and each of their nodes
+ * contain a single digit. Add the two numbers and return it as a linked list.
+ * 
+ * You may assume the two numbers do not contain any leading zero, except the
+ * number 0 itself.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * Output: 7 -> 0 -> 8
+ * Explanation: 342 + 465 = 807.
+ * 
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {    
+        ListNode preHead = new ListNode(-1);
+        ListNode currNode = preHead;
+        int carryOver = 0;
+        while (l1 != null || l2 != null || carryOver > 0) {
+            int sum = carryOver;
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
             }
-            l1 = l1.next;
-            l2 = l2.next;
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }                            
+            ListNode newNode = new ListNode(sum % 10);
+            carryOver = sum / 10;
+            currNode.next = newNode;
+            currNode = newNode;
         }
-
-        ListNode l = (l1 == null) ? l2 : l1;
-        while (l != null) {
-            ListNode newNode = new ListNode(l.val + carry);
-            curr.next = newNode;
-            curr = newNode;
-            if (curr.val >= 10) {
-                curr.val %= 10;
-                carry = 1;
-            } else {
-                carry = 0;
-            }
-            l = l.next;
-        }
-
-        if (carry == 1) {
-            ListNode newNode = new ListNode(1);
-            curr.next = newNode;
-            newNode.next = null;
-        }
-
-        return pre.next;
-    }
-
-    private static void printListNode(ListNode node) {
-        ListNode currNode = node;
-        while (currNode != null) {
-            System.out.print(currNode.val);
-            currNode = currNode.next;
-        }
-        System.out.print("\n");
-    }
-
-    /**
-     * Definition for singly-linked list.
-    **/
-    public static class ListNode {
-        int val;
-        ListNode next;
-        ListNode(int x) { val = x; }
+        return preHead.next;
     }
 }
+
