e6fa024453a3faa6ce34eb013e2da3c21c946318
Rosalio <rosalio@users.noreply.github.com>
2019-05-04 23:10:46 -0700

    Update TwoSum.java

--- Problems/1_Two_Sum/TwoSum.java
+++ Problems/1_Two_Sum/TwoSum.java
@@ -1,47 +1,47 @@
-/**********************************************************************************************************************
+import java.util.Map;
 
-Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
-Example:
-Given nums = [2, 7, 11, 15], target = 9,
-
-Because nums[0] + nums[1] = 2 + 7 = 9,
-return [0, 1].
-
-**********************************************************************************************************************/
-
-import java.util.HashMap;
-
-class TwoSum {
-    public static void main(String[] args) {
-        System.out.println("1. Two Sum");
-
-        int[] input = {2, 7, 11, 15};
-        int target = 9;
-
-        int[] output = twoSum(input, target);
-
-        for (final int n : output) {
-          System.out.println(n);
-        }
-    }
-
-    public static int[] twoSum(int[] nums, int target) {
-        int[] result = {-1, -1};
-        final HashMap<Integer, Integer> map = new HashMap<>();
-
-        for (int i = 0; i < nums.length; ++i) {
-            final int num = nums[i];
-            if (map.containsKey(target - num)) {
-                final int firstIndex = map.get(target - num);
-                result[0] = firstIndex;
-                result[1] = i;
-                return result;
+/*
+ * @lc app=leetcode id=1 lang=java
+ *
+ * [1] Two Sum
+ *
+ * https://leetcode.com/problems/two-sum/description/
+ *
+ * algorithms
+ * Easy (42.17%)
+ * Total Accepted:    1.5M
+ * Total Submissions: 3.6M
+ * Testcase Example:  '[2,7,11,15]\n9'
+ *
+ * Given an array of integers, return indices of the two numbers such that they
+ * add up to a specific target.
+ * 
+ * You may assume that each input would have exactly one solution, and you may
+ * not use the same element twice.
+ * 
+ * Example:
+ * 
+ * 
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * 
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ * 
+ * 
+ * 
+ * 
+ */
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        final Map<Integer, Integer> numToIndexMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            final int currNum = nums[i];
+            final int numToLookup = target - currNum;
+            if (numToIndexMap.containsKey(numToLookup)) {
+                return new int[] {numToIndexMap.get(numToLookup), i};
             }
-            map.put(num, i);
+            numToIndexMap.put(currNum, i);
         }
-        return result;
+        throw new IllegalArgumentException("Result does not exist");
     }
 }
