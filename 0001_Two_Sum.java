33ca754c627a46ff39fee376a919ad939d7645ed
Alex Dai <alex.dai@clio.com>
2019-12-08 22:29:38 -0800

    Problem 1

--- 0001_Two_Sum.java
+++ 0001_Two_Sum.java
@@ -1,13 +1,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> numToIndexMap = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             int num = nums[i];
             int complement = target - num;
-            if (numToIndexMap.containsKey((complement))) {
-                return new int[]{numToIndexMap.get(complement), i};
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
             }
-            numToIndexMap.put(num, i);
+            map.put(num, i);
         }
         throw new IllegalArgumentException("Cannot find such two numbers.");
     }
